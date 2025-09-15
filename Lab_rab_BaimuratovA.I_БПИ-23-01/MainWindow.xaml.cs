f9d208460951c2c5f79a1274f43402e6ad2db01c
Student <verso00o0o1@gmail.com>
2025-09-15 11:23:43 +0500

    Создание функция-метода обработки данных 1

--- Lab_rab_BaimuratovA.I_БПИ-23-01/MainWindow.xaml.cs
+++ Lab_rab_BaimuratovA.I_БПИ-23-01/MainWindow.xaml.cs
@@ -29,8 +29,14 @@ namespace Lab_rab_BaimuratovA.I_БПИ_23_01
     public class Book
     {
         public string Title { get; set; }
-        public int Page { get; set; }
+        public int Pages { get; set; }
         public int Price { get; set; }
 
+
+        public double AverageCostOfPafe()
+        {
+            if (Pages <= 0) return 0;
+            return Price / Pages;
+        }
     }
 }
